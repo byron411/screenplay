d1cc9ebc83b3bb344e6e1b5251c010e35959cbdc
unknown <byron789_@hotmail.com>
2021-11-17 14:41:38 -0500

    Cap 1.1 terminado

--- src/test/java/co/com/choucair/certification/proyectobase/runners/RunnerTags.java
+++ src/test/java/co/com/choucair/certification/proyectobase/runners/RunnerTags.java
@@ -1,4 +1,13 @@
 package co.com.choucair.certification.proyectobase.runners;
-//import cucumber.api.CucumberOptions;
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resouces/features/demo.feature",
+                tags="@tag1",
+                glue="co.com.choucair.certification.proyectobase.stepdefinitions",
+snippets = SnippetType.CAMELCASE)
 public class RunnerTags {
 }
